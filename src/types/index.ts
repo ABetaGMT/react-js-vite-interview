394d23cba2635b2f10e37438795e68bbc5ef6336
A Beta GMT <andresbetancourt@goldmediatech.com>
2025-07-17 22:20:00 -0500

    ⚙ Interview React.js Setup

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Create a TODO Application
+ * Define the corresponding interfaces or more if you like to implement more features
+ */
+export interface User {}
+
+export interface Card {}
+
+export enum Status {}
+
+export enum Filters {}
+
+export interface Task {}
